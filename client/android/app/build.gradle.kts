f3cff02fd8fe9e98ca289e181c9f7104635ef574
Gustavo Menezes Barbosa <gustavomb1313@gmail.com>
2025-05-20 15:08:22 -0300

    configurando build.gradle nivel do app

--- client/android/app/build.gradle.kts
+++ client/android/app/build.gradle.kts
@@ -3,13 +3,17 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    
+    // Add the dependency for the Google services Gradle plugin
+    id("com.google.gms.google-services")
 }
 
 android {
     namespace = "com.example.damd_trabalho_1"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
-
+    // ndkVersion = "27.0.12077973"
+    
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -42,3 +46,5 @@ android {
 flutter {
     source = "../.."
 }
+
+apply(plugin = "com.google.gms.google-services")
